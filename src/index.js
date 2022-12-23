50f4065b6750c88387ecad17681b98139e5998f9
aouttier.ext <arnaud.outtier@protonmail.com>
2022-12-23 02:59:50 +0100

    lo

--- src/index.js
+++ src/index.js
@@ -2,26 +2,29 @@ const siteNavigationMobile = document.querySelector('.site-navigation.mobile');
 const buttonNavToggle = document.querySelectorAll('button.nav-toggle');
 let sideBarOpen = true;
 
-// Toggle site navigatino menu
 
-// hide site navigation when click outside
+// Toggle site navigatino menu
+if (buttonNavToggle.length) {
+    buttonNavToggle.forEach(navToggleButton => {
+        console.log(navToggleButton);
+        navToggleButton.addEventListener('click', () => {
+            siteNavigationMobile.classList.toggle('active');
+        })
+    });
+}
+
+// collapse site navigation when click outside
 document.addEventListener('click', event => {
 
     console.log(sideBarOpen);
 
-    buttonNavToggle.forEach(navToggleButton => {
-        if (navToggleButton.contains(event.target)) {
-            siteNavigationMobile.classList.toggle('active');
-        }
-    });
-
-    if (sideBarOpen) {
+    if (!sideBarOpen) {
         if (!siteNavigationMobile.contains(event.target)) {
             siteNavigationMobile.classList.remove('active');
         }
-        sideBarOpen = false;
+        sideBarOpen = true;
     }
     else {
-        sideBarOpen = true;
+        sideBarOpen = false;
     }
 })
