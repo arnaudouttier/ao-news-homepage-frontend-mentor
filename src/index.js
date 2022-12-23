3e7f273af5aeb18ac87bcc208434ee79ff55a4f2
aouttier.ext <arnaud.outtier@protonmail.com>
2022-12-23 02:46:11 +0100

    link hover

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const siteNavigationMobile = document.querySelector('.site-navigation.mobile');
 const buttonNavToggle = document.querySelectorAll('button.nav-toggle');
+let sideBarOpen = true;
 
 // Toggle site navigatino menu
-if (buttonNavToggle.length) {
-    buttonNavToggle.forEach(navToggleButton => {
-        console.log(navToggleButton);
-        navToggleButton.addEventListener('click', () => {
-            siteNavigationMobile.classList.toggle('active');
-        })
-    });
-}
 
+// hide site navigation when click outside
+document.addEventListener('click', event => {
+
+    console.log(sideBarOpen);
+
+    buttonNavToggle.forEach(navToggleButton => {
+        if (navToggleButton.contains(event.target)) {
+            siteNavigationMobile.classList.toggle('active');
+        }
+    });
+
+    if (sideBarOpen) {
+        if (!siteNavigationMobile.contains(event.target)) {
+            siteNavigationMobile.classList.remove('active');
+        }
+        sideBarOpen = false;
+    }
+    else {
+        sideBarOpen = true;
+    }
+})
