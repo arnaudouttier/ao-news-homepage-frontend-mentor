471ad15e93783a237608179e2bf0d440b6390151
aouttier.ext <arnaud.outtier@protonmail.com>
2022-12-23 13:12:16 +0100

    final

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 const siteNavigationMobile = document.querySelector('.site-navigation.mobile');
 const buttonNavToggle = document.querySelectorAll('button.nav-toggle');
-let sideBarOpen = true;
+let sideBarOpen = false;
 
 
-// Toggle site navigatino menu
-if (buttonNavToggle.length) {
-    buttonNavToggle.forEach(navToggleButton => {
-        console.log(navToggleButton);
-        navToggleButton.addEventListener('click', () => {
-            siteNavigationMobile.classList.toggle('active');
-        })
-    });
-}
 
-// collapse site navigation when click outside
 document.addEventListener('click', event => {
 
-    console.log(sideBarOpen);
+    // Toggle site navigatino menu
+    buttonNavToggle.forEach(navToggleButton => {
 
-    if (!sideBarOpen) {
+        if (navToggleButton.contains(event.target))
+            siteNavigationMobile.classList.toggle('active');
+    });
+
+
+    // hide site navigation sidebar when click outside on mobile
+    if (sideBarOpen) {
         if (!siteNavigationMobile.contains(event.target)) {
             siteNavigationMobile.classList.remove('active');
         }
-        sideBarOpen = true;
     }
-    else {
-        sideBarOpen = false;
-    }
+
+    sideBarOpen = sideBarOpen ? false : true
+
 })
+
+
+
+
